ff85c013712ce77340b05146107534040e32683e
mauriciocepeda <johan_maoc@hotmail.com>
2020-04-11 16:59:25 -0500

    usuario y token listo

--- app.js
+++ app.js
@@ -4,11 +4,26 @@
 
 var express = require('express'); 
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 //nicializar variables
 
 var app = express(); 
+
+
+//body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+//importar rutas
+var appRoutes = require('./routes/app'); 
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 //conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, response)=>{
@@ -16,18 +31,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, resp
     if(error) throw error;
     console.log('Base de datos, online');
 
-})
+});
 
 
 //rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-app.get('/', (request, response, next) =>{
-
-    response.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
 
 
 //escuchar peticiones
