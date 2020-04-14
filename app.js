707448ec0a66d40b47066145460dc15d7502a74d
mauriciocepeda <johan_maoc@hotmail.com>
2020-04-13 20:52:15 -0500

    seccion 12 terminada backend

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(bodyParser.json())
 var appRoutes = require('./routes/app'); 
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 //conexion a la base de datos
@@ -33,10 +38,23 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, resp
 
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
+
 
 //rutas
+app.use('/img', imagenesRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/hospital', hospitalRoutes);
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+
+
 app.use('/', appRoutes);
 
 
--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//google
+module.exports.CLIENT_ID = '841906353135-18ljpao4k9a52d36ihejiv8rkkspijjn.apps.googleusercontent.com';
